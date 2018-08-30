5fea490e3dbe930738319c88d8a156fcf1d79deb
Lancexu883 <ychooyy@gmail.com>
2018-08-30 22:25:39 +0800

    添加冒泡排序 修改了冒泡排序 新加算法

---
+++ Algorithms/src/main/java/algorithms/TreeStudy/Node.java
@@ -0,0 +1,17 @@
+package algorithms.TreeStudy;
+
+/**
+ * @Author: Lance
+ * @Date: 2018/8/30 10:47
+ */
+public class Node
+{
+    private Object data;
+    private Node leftChild;
+    private Node rightChild;
+
+    public void display()
+    {
+        System.out.println(data);
+    }
+}
