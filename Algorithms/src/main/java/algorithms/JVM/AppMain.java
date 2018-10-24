0364bba475a80d36cf1d46b0a5a5e8abe07e9628
LanceXuu <ychooyy@gmail.com>
2018-10-24 18:15:12 +0800

    测试

--- Algorithms/src/main/java/algorithms/JVM/AppMain.java
+++ Algorithms/src/main/java/algorithms/JVM/AppMain.java
@@ -4,8 +4,11 @@ package algorithms.JVM;
  * @Author: Lance
  * @Date: 2018/8/20 21:04
  */
-public class AppMain
-{
+public class AppMain {
+
+    public static void main(String[] args) {
+        System.out.printf("1");
+    }
 
 }
 
